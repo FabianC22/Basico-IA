c939a4ba9eabffe01422bb380a6f4b3401478b46
FabianC22 <99307129+FabianC22@users.noreply.github.com>
2023-02-20 12:23:22 -0500

    Add files via upload

---
+++ Tareas/Presentación y concepto estadistico .py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ![Logo|0.10](http://localhost:8888/tree/Inteligencia%20Artificial/ciencia-de-datos.png)
+
+# ## Presentación
+# Hola, me presento soy Fabian Castellanos soy estudiante de estadística en la Universidad Nacional de Colombia, basicamente mi camino academico esta dirigido al analísis de datos utilizando tecnicas estadísticas he implementación de ciencias de datos, es decir Machine learning y redes neuronales para la solución de problemas de regresión o clasificaciôn. 
+# 
+# ![Logo|0.10](http://localhost:8888/tree/Inteligencia%20Artificial/Foto.jpeg)
+# 
+
+# ## Varianza
+
+# Se definira el concepto de varianza, el cual es muy importante para la estadistíca y para su construcción teorica, basicamente es una medida de dispersión en los datos.
+# 
+# La varianza, $S^2$, se define como la media de las deferencias cuadraticas de $n$ puntuaciones con respecto a su media aritmetica, es decir
+# 
+# $$S^2= \frac{1}{n} \sum_{i=1}^{n} (x_{i}-\bar{x})^2  $$
+# 
+# ### Ejemplo:
+# 
+# Calcula la varianza de las siguientes cantidades:
+# 
+# $3,3,4,4,5$
+# 
+# Solución:
+# 
+# Se calcula la media, $\bar{x}=3,8$ y se aplica la ecuación de la varianza para cada punto observado, lo cual se obtiene la siguiente varianza, $S^2=0.56$
+# 
+# ### Refencias:
+# 
+# [1.Varianza](https://virtual.uptc.edu.co/ova/estadistica/docs/libros/ftp.bioestadistica.uma.es/libro/node22.htm)
+# 
